1599e6046aefc25a68c7c6c31156c62cbc29d1e6
Alef Santos <alefdeoliveirasantos@gmail.com>
2021-02-16 23:25:12 -0300

    need input  validation :wrench:

---
+++ scripts.js
@@ -0,0 +1,31 @@
+const Binario = document.getElementById('inputBinario')
+const Decimal = document.getElementById('inputDecimal')
+
+const bin2dec = input => parseInt(input,2)
+
+const dec2bin = input => {
+    const intInput =  Number(input)
+    let aux = intInput
+    let i = 0
+    let binary = 0
+    const j = Math.ceil(Math.log2(input))
+
+    for (i = 0; i < j; i++){
+        binary += (aux % 2)*(10**i)
+        aux = Math.floor(aux/2)
+    }
+    return binary
+}
+
+function tryConvert() {
+    if (Binario.value != "")  Decimal.value = bin2dec(Binario.value)
+    else Binario.value = dec2bin(Decimal.value)
+}
+
+function clearDec() {
+    Decimal.value = ""
+}
+
+function clearBin() {
+    Binario.value = ""
+}
